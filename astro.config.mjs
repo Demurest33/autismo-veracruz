40f108c2e88594264b4cd9ac0c45471c56fba5d4
javierjararenteria@gmail.com <202160023@ucc.mx>
2024-06-04 12:08:47 -0600

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  site: "https://demurest.github.io",
+  base: "/autismo-veracruz/",
 });
